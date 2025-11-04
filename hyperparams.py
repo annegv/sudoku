6b5058964ea5887e4fd4e7a9e7ae814bb3b8f610
annegv <annegv@login008.inband>
2025-11-03 21:43:44 -0500

    initial orcd setup

--- hyperparams.py
+++ hyperparams.py
@@ -1,7 +1,10 @@
 class Hyperparams:
     '''Hyper parameters'''
     # data
-    train_fpath = '../v2/data/sudoku.csv'
+    # train_fpath = '../v2/data/sudoku.csv'
+    # updated train path for the orcd data setup
+    train_fpath = '../orcd/scratch/sudoku_data/sudoku.csv'
+    # left the test data where it was as given in the cloned repo
     test_fpath = '../v2/data/test.csv'
     
     # model
